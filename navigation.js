d3127811cf0be487ea0bd907babe72f65dfd8192
lucanus-cervus <62880306+lucanus-cervus@users.noreply.github.com>
2020-03-30 14:10:47 +0200

    Add files via upload

--- navigation.js
+++ navigation.js
@@ -5,4 +5,14 @@ toggleMenu.addEventListener('click', function() {
 	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
 	toggleMenu.setAttribute('aria-expanded', !open);
 	menu.hidden = !menu.hidden;
+	document.body.setAttribute("class", "noscroll");
+	const noscroll = document.querySelector('.noscroll');
+	if(menu.hidden)
+	{
+		noscroll.style.overflow = "scroll";
+	}
+	else
+	{
+		noscroll.style.overflow = "hidden";
+	}
 });
